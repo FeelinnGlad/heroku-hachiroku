184757d4ee04baeca23374b3900b3e27db727334
FeelinnGlad <68422874+FeelinnGlad@users.noreply.github.com>
2020-07-20 10:14:12 +0300

    Update mainfile.js

--- mainfile.js
+++ mainfile.js
@@ -2,39 +2,39 @@ const VkBot = require('node-vk-bot-api');
 
 const bot = new VkBot(process.env.TOKEN);
 
-bot.command(/^\d{1,} (\+|\-|\*|\/|\%|\^) \d{1,}$/, (ctx) => {
-    let arr = ctx.split(" ")
+// bot.command(/^\d{1,} (\+|\-|\*|\/|\%|\^) \d{1,}$/, (ctx) => {
+//     let arr = ctx.split(" ")
 
-    switch (arr[1]) {
-        case "+": {
-            ctx.reply(parseFloat(arr[0]) + parseFloat(arr[2]))
-            break
-        }
-        case "-": {
-            ctx.reply(parseFloat(arr[0]) - parseFloat(arr[2]))
-            break
-        }
-        case "*": {
-            ctx.reply(parseFloat(arr[0]) * parseFloat(arr[2]))
-            break
-        }
-        case "/": {
-            ctx.reply(parseFloat(arr[0]) / parseFloat(arr[2]))
-            break
-        }
-        case "**": {
-            ctx.reply(parseFloat(arr[0]) ** parseFloat(arr[2]))
-            break
-        }
-        case "%": {
-            ctx.reply(parseFloat(arr[0]) % parseFloat(arr[2]))
-            break
-        }
-        default: {
-            ctx.reply("Invalid conversion! Try using sample correctly.")
-        }
+//     switch (arr[1]) {
+//         case "+": {
+//             ctx.reply(parseFloat(arr[0]) + parseFloat(arr[2]))
+//             break
+//         }
+//         case "-": {
+//             ctx.reply(parseFloat(arr[0]) - parseFloat(arr[2]))
+//             break
+//         }
+//         case "*": {
+//             ctx.reply(parseFloat(arr[0]) * parseFloat(arr[2]))
+//             break
+//         }
+//         case "/": {
+//             ctx.reply(parseFloat(arr[0]) / parseFloat(arr[2]))
+//             break
+//         }
+//         case "**": {
+//             ctx.reply(parseFloat(arr[0]) ** parseFloat(arr[2]))
+//             break
+//         }
+//         case "%": {
+//             ctx.reply(parseFloat(arr[0]) % parseFloat(arr[2]))
+//             break
+//         }
+//         default: {
+//             ctx.reply("Invalid conversion! Try using sample correctly.")
+//         }
 
-    }
-})
+//     }
+// })
 
 bot.startPolling();
