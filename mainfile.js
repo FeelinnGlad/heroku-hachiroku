680652cd27ce29d546960363875406cf7b16e100
FeelinnGlad <68422874+FeelinnGlad@users.noreply.github.com>
2020-07-20 09:19:00 +0300

    Update mainfile.js

--- mainfile.js
+++ mainfile.js
@@ -3,16 +3,42 @@ const VkBot = require('node-vk-bot-api');
 const bot = new VkBot(process.env.TOKEN);
 
 bot.command('/start', (ctx) => {
-    ctx.reply('Heya!! Enter your conversion.');
+    ctx.reply('Heya!! Enter your conversion by this sample: [number][space][math.operator][space][number].');
 });
 
-// bot.command(/^\d{1,} (\+|\-|\*|\/|\%|\^) \d{1,}$/, ctx => {
-//     ctx.reply
-    // var str = null
+bot.command(/^\d{1,} (\+|\-|\*|\/|\%|\^) \d{1,}$/, (ctx) => {
+    let arr = ctx.split(" ")
 
-// })
+    switch (arr[1]) {
+        case "+": {
+            ctx.reply(parseFloat(arr[0]) + parseFloat(arr[2]))
+            break
+        }
+        case "-": {
+            ctx.reply(parseFloat(arr[0]) - parseFloat(arr[2]))
+            break
+        }
+        case "*": {
+            ctx.reply(parseFloat(arr[0]) * parseFloat(arr[2]))
+            break
+        }
+        case "/": {
+            ctx.reply(parseFloat(arr[0]) / parseFloat(arr[2]))
+            break
+        }
+        case "**": {
+            ctx.reply(parseFloat(arr[0]) ** parseFloat(arr[2]))
+            break
+        }
+        case "%": {
+            ctx.reply(parseFloat(arr[0]) % parseFloat(arr[2]))
+            break
+        }
+        default: {
+            ctx.reply("Invalid conversion! Try using sample correctly.")
+        }
 
-// var floor = Math.floor;
-// var x = floor("1000.01");
+    }
+})
 
 bot.startPolling();
