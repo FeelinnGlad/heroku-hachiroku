df3e0b57ea71f28710deb6690fce2cd414122ff8
FeelinnGlad <68422874+FeelinnGlad@users.noreply.github.com>
2020-07-21 14:12:06 +0300

    Update mainfile.js

--- mainfile.js
+++ mainfile.js
@@ -6,37 +6,39 @@ const bot = new VkBot('175f16b9f6336969610f7b922ea53221ede242b044ffefa27a56fd29d
 bot.command(/^\d{1,} (\+|\-|\*|\/|\%|\^) \d{1,}$/, (ctx) => {
 
     let arr = ctx.message.text.split(" ")
-
-    switch (arr[1]) {
-        case "+": {
-            ctx.reply(parseFloat(arr[0]) + parseFloat(arr[2]))
-            break
+    
+    try {
+        switch (arr[1]) {
+            case "+": {
+                ctx.reply(parseFloat(arr[0]) + parseFloat(arr[2]))
+                break
+            }
+            case "-": {
+                ctx.reply(parseFloat(arr[0]) - parseFloat(arr[2]))
+                break
+            }
+            case "*": {
+                ctx.reply(parseFloat(arr[0]) * parseFloat(arr[2]))
+                break
+            }
+            case "/": {
+                ctx.reply(parseFloat(arr[0]) / parseFloat(arr[2]))
+                break
+            }
+            case "^": {
+                ctx.reply(parseFloat(arr[0]) ** parseFloat(arr[2]))
+                break
+            }
+            case "%": {
+                ctx.reply(parseFloat(arr[0]) % parseFloat(arr[2]))
+                break
+            }
         }
-        case "-": {
-            ctx.reply(parseFloat(arr[0]) - parseFloat(arr[2]))
-            break
-        }
-        case "*": {
-            ctx.reply(parseFloat(arr[0]) * parseFloat(arr[2]))
-            break
-        }
-        case "/": {
-            ctx.reply(parseFloat(arr[0]) / parseFloat(arr[2]))
-            break
-        }
-        case "^": {
-            ctx.reply(parseFloat(arr[0]) ** parseFloat(arr[2]))
-            break
-        }
-        case "%": {
-            ctx.reply(parseFloat(arr[0]) % parseFloat(arr[2]))
-            break
-        }
-        default: {
-            ctx.reply("Invalid conversion! Try using sample correctly.")
-        }
-
+        
+    } catch (e) {
+        console.log(e)
     }
+
 })
 
 console.log('Бот запущен')
